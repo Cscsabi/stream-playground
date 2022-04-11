3c33de7f2e742ade61f9f465d5874a51058b8aa8
Csaba Csizmadia <cscsabi2001@gmail.com>
2022-04-11 13:12:43 +0200

    Homework: using java streams (2)

--- src/main/java/brickset/LegoSetRepository.java
+++ src/main/java/brickset/LegoSetRepository.java
@@ -2,8 +2,7 @@ package brickset;
 
 import repository.Repository;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -54,9 +53,9 @@ public class LegoSetRepository extends Repository<LegoSet> {
     }
 
     /**
-     * Returns a Map object wrapping a summary of the used packaging types and their frequency among LEGO sets.
+     * Returns a Map object wrapping the summary of the used packaging types and their frequency among LEGO sets.
      *
-     * @return a {@code Map<PackagingType, Long>} object wrapping a summary of the used packaging types
+     * @return a {@code Map<PackagingType, Long>} object wrapping the summary of the used packaging types
      * and their frequency among LEGO sets.
      */
     public Map<PackagingType, Long> getPackagingTypeSummary() {
@@ -74,9 +73,68 @@ public class LegoSetRepository extends Repository<LegoSet> {
                 .ifPresent(System.out::println);
     }
 
+    /**
+     * Returns whether each legoset has fewer pieces than five hundred.
+     *
+     * @return whether every legoset has fewer pieces than five hundred.
+     */
+    public boolean returnIfAllSetsHaveFewerPiecesThanFiveHundred() {
+        return getAll().stream()
+                .map(LegoSet::getPieces)
+                .allMatch(piece -> piece <= 500);
+    }
+
+    /**
+     * Prints every distinct tag sorted to the console, that have no subthemes.
+     */
+    public void printAllSortedDistinctTagsThatHaveNoSubtheme() {
+        getAll().stream()
+                .filter(brickset -> brickset.getSubtheme() != null && brickset.getTags() != null)
+                .flatMap(brickset ->  brickset.getTags().stream())
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    /**
+     * Returns the theme, that has the longest name.
+     *
+     * @return {@code Optional<String>} object, that should contain the theme with the longest name.
+     */
+    public Optional<String> getThemeWithTheLongestName() {
+        return getAll().stream()
+                .map(LegoSet::getTheme)
+                .reduce((firstTheme, secondTheme) ->
+                        firstTheme.length() > secondTheme.length() ? firstTheme : secondTheme);
+    }
+
+    /**
+     * Returns a Map object, that contains a summary of the legosets' themes and their frequency.
+     *
+     * @return {@code Map<String, Long>} object wrapping how many legosets have the same theme.
+     */
+    public Map<String, Long> getNumberOfSetsForEachTheme() {
+        return getAll().stream()
+                .collect(Collectors.groupingBy(LegoSet::getTheme, Collectors.counting()));
+    }
+
+    /**
+     * Returns a Map object, that contains every theme and their distinct subthemes.
+     *
+     * @return {@code Map<String, Set<String>>} object wrapping the legosets' themes and their subthemes.
+     */
+    public Map<String, Set<String>> getMapOfThemesWithTheirSubthemes() {
+        return getAll().stream()
+                .collect(Collectors.groupingBy(LegoSet::getTheme,
+                        Collectors.mapping(LegoSet::getSubtheme,
+                        Collectors.filtering(Objects::nonNull,
+                        Collectors.toSet()))));
+    }
+
     public static void main(String[] args) {
         var repository = new LegoSetRepository();
 
+        /* Previous homework
         System.out.println("Lego names starting with the String 'lava':");
         repository.printLegoSetsStartingWithGivenString("lava");
 
@@ -91,5 +149,21 @@ public class LegoSetRepository extends Repository<LegoSet> {
 
         System.out.println("\nThe sum of all lego pieces:");
         repository.printSumOfLegoPieces();
+        */
+
+        System.out.println("\nDo all sets have less pieces than five hundred?");
+        System.out.println(repository.returnIfAllSetsHaveFewerPiecesThanFiveHundred());
+
+        System.out.println("\nSorted, distinct tags of legosets, that have no subtheme:");
+        repository.printAllSortedDistinctTagsThatHaveNoSubtheme();
+
+        System.out.println("\nGet the longest theme name:");
+        repository.getThemeWithTheLongestName().ifPresent(System.out::println);
+
+        System.out.println("\nGet number of sets for each theme:");
+        System.out.println(repository.getNumberOfSetsForEachTheme());
+
+        System.out.println("\nGet each theme with their distinct subthemes:");
+        System.out.println(repository.getMapOfThemesWithTheirSubthemes());
     }
 }
